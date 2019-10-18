5eaddf4f2150457f8cc4bb7456a59d8cbeb572d1
中成才の币兜 <chenai.nakam@gmail.com>
2019-10-19 01:13:02 +0800

    upd > web.models.

--- src/main/scala/com/creditid/cid/operations/SignOff.scala
+++ src/main/scala/com/creditid/cid/operations/SignOff.scala
@@ -4,13 +4,9 @@ import cats.Applicative
 import com.creditid.cid.web.models._
 
 trait SignOff[F[_]]{
-  //1) 凭证提供机构注册上链（凭证提供机构的公钥地址上链）
   def post(n: OrgKey): F[Option[机构ID]]
-  //2) 获取凭证注册时凭证编号对应的公钥对_
-  def get(n: 机构ID): F[Option[公钥数组]]
-  //3)新签发的凭证，明文哈希和凭证上链
+  def get(n: 机构ID): F[Option[公钥组]]
   def post(n: UpLink): F[Option[UpLinkResult]]
-  //4)注销凭证
   def delete(n: 凭证的链上ID): F[Option[凭证状态]]
 }
 
@@ -19,11 +15,10 @@ object SignOff {
   def apply[F[_] : Applicative]: SignOff[F] = new SignOff[F] {
     override def post(n: OrgKey): F[Option[机构ID]] = ???
 
-    override def get(n: 机构ID): F[Option[公钥数组]] = ???
+    override def get(n: 机构ID): F[Option[公钥组]] = ???
 
     override def post(n: UpLink): F[Option[UpLinkResult]] = ???
 
     override def delete(n: 凭证的链上ID): F[Option[凭证状态]] = ???
   }
-
 }
--- src/main/scala/com/creditid/cid/web/models/package.scala
+++ src/main/scala/com/creditid/cid/web/models/package.scala
@@ -2,44 +2,106 @@ package com.creditid.cid.web
 
 import cats.kernel.Eq
 import io.circe.generic.JsonCodec
-
+import com.creditid.cid.web.models.ResqCode._
+import shapeless.:+:
 
 package object models {
+  type JOBJ = (String, JOBJ :+: String)
+  type JARR = Seq[JOBJ]
   type CID = String
-  //备注：CID是自然的身份ID。每个凭证都是属于某一个CID的。
   type 机构ID = String
-  type Records = String
-  type PersonalDetail = String
-  type 公钥数组 = String
-  type 凭证指纹 = String
-  type 凭证明文 = String
-  type 凭证有效期 = String
-  type 凭证的链上ID = String
-  type 机构签名 = String
-  type 凭证状态 = String
+  type 个人信息 = JOBJ
+  type 记录 = JOBJ
+  type 公钥 = String
+  type 公钥组 = JARR
+  type 凭据 = JOBJ
+  type 签名 = String
+  type 凭据状态 = Credit.状态
+  type 返回状态 = ResqCode.tpe
+  type 随机数用途 = Random.用途
+  type 凭据JOBJ = (("status".type, 凭据状态), 凭据)
 
-  @JsonCodec
-  final case class Register(cid: CID, details: PersonalDetail)
+  object Credit extends Enumeration {
+    type 状态 = State
 
-  @JsonCodec
-  final case class Details(cid: CID, records: Records)
+    case class State(value: String) extends Val
 
-  @JsonCodec
-  final case class OrgKey(orgId: 机构ID, key: 公钥数组)
+    val 有效 = State("valid")
+    val 无效 = State("invalid")
+    val 不存在 = State("not exist")
+  }
 
+  object Random extends Enumeration {
+    type 用途 = Usage
 
-  @JsonCodec
-  final case class UpLink(cid: CID, orgId: 机构ID, fingerprint: 凭证指纹, text: 凭证明文, limit: 凭证有效期)
+    case class Usage(value: String) extends Val
 
-  implicit val upLinkEq = Eq.fromUniversalEquals[UpLink]
-  @JsonCodec
-  final case class UpLinkResult(upLink: UpLink, address: 凭证的链上ID)
+    val 请求CreditUse接口 = Usage("credit_use")
+  }
 
-  @JsonCodec
-  final case class ValidationInfo(address: 凭证的链上ID, signature: 机构签名)
+  object ResqCode extends Enumeration {
+    type tpe = Code
 
+    case class Code(code: Int, desc: String) extends Val
 
-  @JsonCodec
-  final case class ValidateResult(status: 凭证状态, fingerprint: 凭证指纹, text: 凭证明文)
+    val 执行成功 = Code(0x1, "执行成功")
+    val 验签失败 = Code(0x2, "验签失败")
+  }
+
+  object request {
+
+    @JsonCodec
+    final case class org_register(org_id: 机构ID, pubkeys: 公钥组, sign: 签名) // pubKey[].map(->privKey).foreach(sign(org_id))
+
+    @JsonCodec
+    final case class org_upd_pubkey(org_id: 机构ID, pubkey: 公钥, sign: 签名) // pubKey.map(->privKey).foreach(sign(org_id))
+
+    @JsonCodec
+    final case class org_get_pubkeys(org_id: 机构ID)
+
+    @JsonCodec
+    final case class cid_register(cid: CID, data: 个人信息, sign: 签名) // sign(hash(cid + data))
+
+    @JsonCodec
+    final case class cid_record(cid: CID, data: 记录, sign: 签名) // sign(hash(cid + data))
+
+    @JsonCodec
+    final case class credit_register(cid: CID, org_id: 机构ID, data: 凭据, sign: 签名) // sign(hash(cid + org_id + data))
+
+    @JsonCodec
+    final case class credit_destroy(cid: CID, org_id: 机构ID, sign: 签名) // sign(hash(cid + org_id))
+
+    @JsonCodec
+    final case class credit_use(cid: CID, org_id: 机构ID, sign: 签名) // sign(random + cid + org_id)
+
+    @JsonCodec
+    final case class random(usage: 随机数用途)
+
+    // TODO: 验签
+  }
+
+  object response {
+
+    final case class org_register(state: 返回状态)
+
+    final case class org_upd_pubkey(state: 返回状态)
+
+    final case class org_get_pubkeys(either: Either[验签失败.type, 公钥组])
+
+    final case class cid_register(state: 返回状态)
+
+    final case class cid_record(state: 返回状态)
+
+    final case class credit_register(state: 返回状态)
+
+    final case class credit_destroy(state: 返回状态)
+
+    final case class credit_use(either: Either[验签失败.type, 凭据JOBJ])
+
+    final case class random(random: BigInt)
+
+  }
+
+  implicit val xxxEq = Eq.fromUniversalEquals[Xxx]
 
 }
